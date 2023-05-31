5be47ee1d4f7bf0d65a3477e2a8de258ad33e040
abhishek gupta <ak4838960@gmail.com>
2023-05-31 23:35:56 +0530

    late night commit

--- src/pages/api/question/deleteQuestion.js
+++ src/pages/api/question/deleteQuestion.js
@@ -10,7 +10,7 @@ export default async function handler(req, res) {
             const collection = db.collection(req.body.trade); // creating collection with name of trade
 
             // const questions = await collection.find({"_id":new ObjectId("6440320c2590bb362173e95d")}).project({_id:1,trade:1,"que.question":1}).toArray()
-            const deleted = await collection.deleteOne({ "_id": new ObjectId("6440320c2590bb362173e95d") })
+            const deleted = await collection.deleteOne({ "_id": new ObjectId(req.body.id) })
 
             res.status(200).json({ ok: "yes ok hai", deleted });
             return res.end();
