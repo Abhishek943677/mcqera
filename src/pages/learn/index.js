c78f758c32eea85c1cc30d9abbda1a90012490a3
abhishek gupta <ak4838960@gmail.com>
2023-07-04 00:07:41 +0530

    late night commit

--- src/pages/learn/index.js
+++ src/pages/learn/index.js
@@ -3,18 +3,18 @@ import { mongoConnectLearn } from "../../../lib/mongoConnectLearn";
 import Link from "next/link";
 
 const Home = ({ lessonsObj }) => {
-  console.log(JSON.parse(lessonsObj));
+  // console.log(JSON.parse(lessonsObj));
 
   return (
     <div>
-      {JSON.parse(lessonsObj).map((item) => {
+      {JSON.parse(lessonsObj).map((item,index) => {
         return (
-          <div className="p-2 rounded-md my-3 make-com-dark">
+          <div className="p-2 rounded-md my-3 make-com-dark" key={index}>
             <p className="text-center text-2xl py-1">{item.course}</p>
             <div>
-              {item.subjectArray.map((subject) => {
+              {item.subjectArray.map((subject,index) => {
                 return (
-                  <div className="mb-1 hover:opacity-50 cursor-pointer">
+                  <div className="mb-1 hover:opacity-50 cursor-pointer" key={index}>
                     <Link href={`learn/${item.course}/${subject.subject}`} className="w-full">
                       {subject.subject}
                     </Link>
